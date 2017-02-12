dac45d80081aced62f4d7d299aaaca13df7f4983
mjyadav <mrityunjay.yadav0007@gmail.com>
2017-02-12 17:51:26 +0530

    assignment1.2
    assignment1.2

---
+++ src/assignment2/SecondCode.java
@@ -0,0 +1,19 @@
+package assignment2;
+
+public class SecondCode {
+
+	public static void main(String[] args) {
+		int a=20; 
+		int b= a-- - --a; 
+		int c=a--;
+        int d=a>>2; 
+        int e=a&b;
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(e);
+        
+
+	}
+
+}
