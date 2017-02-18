c030a83ec397a25faf6f23973080c567be5bc01d
mjyadav <mrityunjay.yadav0007@gmail.com>
2017-02-18 17:14:39 +0530

    assignement 1.2
    assignement 1.2

--- src/assignment2/SecondCode.java
+++ src/assignment2/SecondCode.java
@@ -1,19 +1,27 @@
 package assignment2;
 
+/**
+ * 
+ * This is a given program and we have to find its output.
+ *
+ */
+
 public class SecondCode {
 
 	public static void main(String[] args) {
+		
+		//variable declaration
 		int a=20; 
 		int b= a-- - --a; 
 		int c=a--;
         int d=a>>2; 
         int e=a&b;
+        
+        //print to console
         System.out.println(b);
         System.out.println(c);
         System.out.println(d);
         System.out.println(e);
-        
-
 	}
 
 }
